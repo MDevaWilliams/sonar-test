b36367684f78a0c4c339c196941317bf3fddb8a9
MDevaWilliams <107609876+MDevaWilliams@users.noreply.github.com>
2025-01-09 18:44:14 +0530

    Create sampleTest.java

---
+++ sampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SampleAppTest {
+    @Test
+    public void testDivideNumbers() {
+        assertEquals(2, SampleApp.divideNumbers(4, 2));
+    }
+}
